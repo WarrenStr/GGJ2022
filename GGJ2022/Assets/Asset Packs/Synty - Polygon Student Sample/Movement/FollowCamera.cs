e824a2c771717d71f834d507a25ae849be3808bf
Warren <59877718+WarrenStr@users.noreply.github.com>
2022-01-28 21:35:42 -0500

    Walk Movement

---
+++ GGJ2022/Assets/Asset Packs/Synty - Polygon Student Sample/Movement/FollowCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float  speed;
+
+    private void Start()
+    {
+    
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = target.position;
+
+        if (Input.GetMouseButton(2))
+        {
+            transform.LookAt(target);
+
+            Vector3 mousePos = Input.mousePosition.normalized;
+            Vector3 rotateAxis = new Vector3(0, mousePos.y, 0);
+            if(mousePos.y > 0.5f)
+            {
+                transform.Rotate(rotateAxis * speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.Rotate(rotateAxis * -speed * 2 *Time.deltaTime);
+            }
+            
+
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(Input.mousePosition.normalized);
+        }
+    }
+}
---
+++ GGJ2022/Assets/Asset Packs/Synty - Polygon Student Sample/Movement/Mover.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    NavMeshAgent playerNavAgent;
+    private Animator animator;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerNavAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+    }
+
+    void MoveToCursor()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            playerNavAgent.destination = hit.point;
+        }
+    }
+
+    void UpdateAnimator()
+    {
+        Vector3 navVelocity = playerNavAgent.velocity;
+        Vector3 localNavVelocity = transform.InverseTransformDirection(navVelocity);
+        float speed = localNavVelocity.z;
+        animator.SetFloat("Blend", speed);
+    }
+}
