467608f26e65803cdf280cf60950df71edde87f1
Tricenatop <57019269+Tricenatop@users.noreply.github.com>
2022-01-30 15:07:43 -0500

    main menu
    Added:
    button img
    menu Anim

--- GGJ2022/Assets/_Scripts/LevelManager.cs
+++ GGJ2022/Assets/_Scripts/LevelManager.cs
@@ -5,9 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    public Animator MenuAnim;
+    public void startAnim()
+    {
+        MenuAnim.SetBool("StartAnim", true);
+        StartCoroutine(switchScene());
+    }
+
+    private IEnumerator switchScene()
+    {
+        yield return new WaitForSeconds(2.1f);
+        SceneManager.LoadScene(1);
+    }
+
     public void SwitchToScene(string nameOfScene)
     {
-        print("hi");
         SceneManager.LoadScene(nameOfScene);
     }
 
